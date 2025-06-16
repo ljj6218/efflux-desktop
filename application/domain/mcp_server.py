9840e7196edc1997522dd84593552c9abd4ce0f3
siyong7 <yongsi@isoftstone.com>
2025-06-16 14:20:50 +0800

    增加artifacts逻辑

--- application/domain/mcp_server.py
+++ application/domain/mcp_server.py
@@ -3,24 +3,22 @@ from typing import Optional, Dict, Any, List
 
 class MCPServer(BaseModel):
     """mcp server 领域对象"""
-    # mcp server ID
-    # server_id: Optional[str]
     # mcp server 名字
     server_name: str = None
     # mcp server 描述
     server_description: Optional[str] = None
-    # mcp server 是否应用
-    applied: bool = False
     # mcp server 是否启用
-    enabled: bool = True
+    applied: Optional[bool] = False
+    # mcp server 是否启用
+    enabled: Optional[bool] = True
     # tag
     server_tag: Optional[str] = None
     # 授权自动执行
-    execute_authorization: bool = False
+    execute_authorization: Optional[bool] = False
     # mcp server 配置 env list
-    env: Dict[str, str] = None
+    env: Optional[Dict[str, str]] = None
     # mcp server 配置 arg list
-    args: List[str] = None
+    args: Optional[List[str]] = None
     # mcp server 配置 command
-    command: str = None
+    command: Optional[str] = None
 
