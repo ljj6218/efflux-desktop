5a393a0bd29b0e613f02cb4a84c1535669461f71
siyong7 <yongsi@isoftstone.com>
2025-06-20 15:55:31 +0800

    update setting for macos

--- common/utils/markdown_util.py
+++ common/utils/markdown_util.py
@@ -1,6 +1,11 @@
-from common.utils.file_util import get_resource_path
+from common.utils.file_util import get_resource_path, check_file_and_create
 
 def read(md_url: str) -> str:
     with open(get_resource_path(md_url), 'r', encoding='utf-8') as file:
         content = file.read()
     return content
+
+def write(md_url: str, content: str) -> None:
+    check_file_and_create(get_resource_path(md_url))
+    with open(get_resource_path(md_url), 'w', encoding='utf-8') as file:
+        file.write(content)
