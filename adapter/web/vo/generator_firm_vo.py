766aa4c56efe62219fe6662abeab86d7be6c461f
梁俊景 <617215009@qq.com>
2025-07-01 13:58:22 +0800

    fix：fields 返回值

--- adapter/web/vo/generator_firm_vo.py
+++ adapter/web/vo/generator_firm_vo.py
@@ -10,8 +10,15 @@ class GeneratorFirmResultVo(BaseModel):
     name: str
     base_url:Optional[str] = None
     api_key: Optional[str] = None
+    fields: Optional[dict] = {}
 
     @classmethod
     def from_generator_firm(cls, generator_firm: GeneratorFirm):
         api_key_value: str = generator_firm.api_key.resolve_value() if generator_firm.api_key else None
-        return GeneratorFirmResultVo(id=generator_firm.id, name=generator_firm.name, base_url=generator_firm.base_url, api_key=api_key_value)
+        return GeneratorFirmResultVo(
+            id=generator_firm.id,
+            name=generator_firm.name,
+            base_url=generator_firm.base_url,
+            api_key=api_key_value,
+            fields=generator_firm.fields or {},
+        )
