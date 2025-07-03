06175ef39f0397b29d09f839cd01d4aa1e0ca6f7
梁俊景 <617215009@qq.com>
2025-07-03 11:16:39 +0800

    init demo

---
+++ streamable_server.py
@@ -0,0 +1,11 @@
+from fastmcp import FastMCP
+
+mcp = FastMCP("Demo 🚀")
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+if __name__ == "__main__":
+    mcp.run(transport="streamable-http", host="0.0.0.0", port=8000, path="/mcp")
