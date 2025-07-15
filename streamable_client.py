d0ad676bb1a7e5e2db625f6bbf83bb8359441c23
梁俊景 <617215009@qq.com>
2025-07-15 16:14:05 +0800

    fastmcp demo

--- streamable_client.py
+++ streamable_client.py
@@ -1,13 +1,28 @@
-
 import asyncio
-from fastmcp import Client
+from fastmcp.client import Client
+from fastmcp.client.transports import SSETransport
 
-async def example():
-    async with Client("http://127.0.0.1:9000/mcp") as client:
-        await client.ping()
-        b = await client.call_tool('greet',{'name':'1111'})
-        print(b)
+async def main():
+    server_url = "http://127.0.0.1:8000/sse/"
+    async with Client(transport=SSETransport(server_url, headers={"X-DEMO-HEADER": "ABC"})) as client:
+        # 测试读取资源
+        raw_result = await client.read_resource("request://headers")
+        import json
+        json_result = json.loads(raw_result[0].text)
+        print("Headers from resource:")
+        print(raw_result)
+        print(json_result)
+
+        # 测试调用工具
+        result = await client.call_tool("get_headers_tool")
+        print("Headers from tool:")
+        print(result.data)
+
+        # 测试获取提示
+        result = await client.get_prompt("get_headers_prompt")
+        json_result = json.loads(result.messages[0].content.text)
+        print("Headers from prompt:")
+        print(json_result)
 
 if __name__ == "__main__":
-    asyncio.run(example())
-
+    asyncio.run(main())
--- streamable_server.py
+++ streamable_server.py
@@ -1,14 +1,37 @@
-from fastmcp import FastMCP
+from fastmcp.server import FastMCP
+from fastmcp.server.dependencies import get_http_request
 
-mcp = FastMCP("My MCP Server")
-# @mcp.custom_route("/health", methods=["GET"])
-@mcp.tool()
-def greet(name: str) -> str:
-    print(1111)
-    return f"Hello, {name}! 我是超人"
+def create_server():
+    server = FastMCP(name="SSE Demo Server", instructions="A simple SSE demo server.")
+
+    # 添加一个工具
+    @server.tool
+    def get_headers_tool() -> dict[str, str]:
+        """Get the HTTP headers from the request."""
+        request = get_http_request()
+        data = dict(request.headers)
+        data['x'] = '2'
+        return data
+
+    # 添加一个资源
+    @server.resource(uri="request://headers")
+    async def get_headers_resource() -> dict[str, str]:
+        request = get_http_request()
+        data = dict(request.headers)
+        data['x'] = '1'
+        return data
+
+    # 添加一个提示
+    @server.prompt
+    def get_headers_prompt() -> str:
+        """Get the HTTP headers from the request."""
+        request = get_http_request()
+        data = dict(request.headers)
+        data['x'] = '3'
+        import json
+        return json.dumps(data)
+
+    return server
 
 if __name__ == "__main__":
-    mcp.run(transport="streamable-http", host="127.0.0.1", port=9000)
-
-
-
+    create_server().run(transport="sse", host="0.0.0.0", port=8000, path="/sse")
